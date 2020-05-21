10c62ed9e0aa884af2db94eb0a150935e157106f
unknown <locbgiang@gmail.com>
2020-05-20 19:28:25 -0700

    finished

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,40 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  if(item.enhancement >= 20){
+    return {...item};
+  } else {
+    item.enhancement++;
+    return { ...item };
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if(item.enhancement < 15){ // item failed while bellow 15 enchancement
+    item.durability = item.durability - 5;
+    if(item.durability < 0 ){ // Durability cannot be bellow 0
+      item.durability = 0;
+    }
+    return { ...item };
+
+  } else if (item.enhancement >= 15 ) {  // item failed while 15 or above enchancement
+    item.durability = item.durability - 10;
+    if(item.durability < 0 ){ // Durability cannot be bellow 0
+      item.durability = 0;
+    }
+    if(item.enhancement > 16){ //lowering enhancement 
+      item.enhancement--;
+    }
+    return {...item};
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  if(item.durability > 100){
+    return {...item};
+  }
+  item.durability = 100;
+  return {...item};
 }
 
 function get(item) {
